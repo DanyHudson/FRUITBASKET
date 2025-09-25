fe547857de619be81ad52963d9ec1645ffb367dc
DanyHudson <mail.d.hudson@gmail.com>
2025-09-25 16:16:44 +0200

    added fruitlist component

---
+++ src/app/fruitlist/fruitlist.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fruitlist',
+  imports: [],
+  templateUrl: './fruitlist.html',
+  styleUrl: './fruitlist.scss'
+})
+export class Fruitlist {
+
+}
